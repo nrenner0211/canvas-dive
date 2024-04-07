83d1698252dc8309ebfb7c30589020b059c8d0b7
nrenner <nrenner0211@gmail.com>
2024-04-07 05:01:43 -0400

    canvas tester

---
+++ script.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+    const canvas = document.getElementById('canvasEl');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+    flowField.animate();
+}
+
+class FlowFieldEffect {
+    #ctx;
+    #width;
+    #height;
+    constructor(ctx, width, height){
+        this.#ctx = ctx;
+        this.#ctx.strokeStyle = 'white';
+        this.#width = width;
+        this.#height = height;
+    }
+    #draw(x, y){
+        const length = 300;
+        this.#ctx.beginPath();
+        this.#ctx.moveTo(x, y);
+        this.#ctx.lineTo(x + length, y + length);
+        this.#ctx.stroke();
+    }
+    animate(){
+        this.#draw(100, 100);
+        requestAnimationFrame(this.animate);
+        console.log('animate');
+    }
+}
